2e5f6e44f3092a61c07cc159ef398ea631352c90
Rael <anameofanarcha@gmail.com>
2026-06-16 22:21:14 +0700

    Update navigation to display application name
    Replaced application logo with application name.
    
    fix: application-logo component error

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
             <div class="flex">
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <span class="text-xl font-bold">My Application</span>
                     </a>
                 </div>
 
