5d1cb76be6e864699f538fa24d2a3e9b2ffd5789
NasywaKayanap <kayananasywa70@gmail.com>
2026-06-17 18:27:12 +0700

    Add AdminUserSeeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'mommyybakery@gmail.com'],
+            [
+                'name'              => 'Admin Mommy Bakery',
+                'password'          => Hash::make('Warik123#'),
+                'phone'             => '6282322496181',
+                'is_admin'          => true,
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+}
