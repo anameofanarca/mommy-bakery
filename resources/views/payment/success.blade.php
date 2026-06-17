f555bf247c7ed3e085254728df15e8f547e513c8
NasywaKayanap <kayananasywa70@gmail.com>
2026-06-17 22:28:38 +0700

    Add JavaScript file to success page

--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pesanan Berhasil - Mommy Catering & Bakery</title>
 
-    @vite(['resources/css/app.css', 'resources/css/menu.css'])
+    @vite(['resources/css/app.css', 'resources/css/menu.css', 'resources/js/app.js'])
 </head>
 <body>
     @include('layouts.header')
